587c03887aa79a71a7e0318b9949cc342937c54e
thinction-a <jakuro94@gmail.com>
2020-08-27 19:12:58 +0900

    2020.08.27 #1-4 4:01

--- src/clientState.js
+++ src/clientState.js
@@ -1,3 +1,35 @@
-export const defaults = {};
-export const resolvers = {};
-export const typeDefs = {};
+export const defaults = {
+    notes: [
+        {
+            __typename: "Note",
+            id: 1,
+            title: "First",
+            content: "Second"
+        }
+    ]
+};
+export const typeDefs = [
+    `
+    schema {
+        query: Query
+        mutation: Mutation
+    }
+    type Query {
+        notes: [Note]!
+        note(id: Int!): Note
+    }
+    type Mutation {
+        createNote(title: String!, content: String!): Note
+        editNote(id: String!, title: String!, content: String!): Note
+    }
+    type Note {
+        id: Int!
+        title: String!
+        content: String!
+    }
+    `
+];
+export const resolvers = {
+    Mutation: {},
+    Query: {}
+};
